3263fa1639667d012fcbbf442fcacc15f309a8e1
kboysen <kboysen@1627bae4-3225-46cb-a19f-684862636fd9>
2008-06-06 19:45:25 +0000

    Moving the JMLAnnotations project from JML3 to top level: moving the project

---
+++ JMLAnnotations/trunk/JMLAnnotations/src/org/jmlspecs/annotations/Ghost.java
@@ -0,0 +1,13 @@
+package org.jmlspecs.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Ghost {
+	
+	String value();
+
+}
---
+++ JMLAnnotations/trunk/JMLAnnotations/src/org/jmlspecs/annotations/InitiallyDefinitions.java
@@ -0,0 +1,11 @@
+package org.jmlspecs.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface InitiallyDefinitions {
+  Initially[] value() default {};
+}
