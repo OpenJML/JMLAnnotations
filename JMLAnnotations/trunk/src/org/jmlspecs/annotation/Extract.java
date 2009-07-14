263424f201895e576a8a696bff65de1ddcf14810
davidcok <davidcok@1627bae4-3225-46cb-a19f-684862636fd9>
2009-07-14 23:26:54 +0000

    Changed all uses and guises of org.jmlspecs.annotations to org.jmlspecs.annotation and changed to using the JMLAnnotations SVN repository for annotations.  Un-deprecated @Model.

--- JMLAnnotations/trunk/src/org/jmlspecs/annotation/Extract.java
+++ JMLAnnotations/trunk/src/org/jmlspecs/annotation/Extract.java
@@ -1,11 +1,12 @@
 package org.jmlspecs.annotation;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Documented;
+/** Defines the 'extract' JML annotation */
 
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Model {
-	String value() default "";
+public @interface Extract {
+
 }
--- JMLAnnotations/trunk/src/org/jmlspecs/annotation/Secret.java
+++ JMLAnnotations/trunk/src/org/jmlspecs/annotation/Secret.java
@@ -1,11 +1,13 @@
 package org.jmlspecs.annotation;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Documented;
+
+/** Defines the 'secret' JML annotation */
 
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Model {
-	String value() default "";
+public @interface Secret {
+    String value() default "";
 }
