d22062cadf3420141eeaff2fa2249619126a7843
jsinglet <jsinglet@1627bae4-3225-46cb-a19f-684862636fd9>
2013-09-05 18:30:31 +0000

    Modified the Level and Channel annotations to be more restrictive about their placement.

--- src/org/jmlspecs/annotation/Channel.java
+++ src/org/jmlspecs/annotation/Channel.java
@@ -1,10 +1,11 @@
 package org.jmlspecs.annotation;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 
 @Documented
+@Target({ ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD })
 public @interface Channel {
     public static final String DEFAULT = "__JML_CHANNEL_INTERNAL_DEFAULT";
     //@ Note that NO application of this annotation should have a blank level (or channel)
--- src/org/jmlspecs/annotation/Level.java
+++ src/org/jmlspecs/annotation/Level.java
@@ -1,9 +1,11 @@
 package org.jmlspecs.annotation;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 
 @Documented
-
+@Target({ ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.FIELD, ElementType.METHOD })
 public @interface Level {
     public static final String DEFAULT = "__JML_LEVEL_INTERNAL_DEFAULT";
     //@ Note that NO application of this annotation should have a blank level (or channel)
