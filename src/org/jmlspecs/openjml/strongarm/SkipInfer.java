af2780ffdd1712375b747f3e9084b79db42747cb
John L. Singleton <jsinglet@gmail.com>
2017-03-19 19:58:32 -0400

    allowed for constructors

--- src/org/jmlspecs/openjml/strongarm/SkipInfer.java
+++ src/org/jmlspecs/openjml/strongarm/SkipInfer.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 //@Retention(RetentionPolicy.SOURCE)
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
 @Documented
 public @interface SkipInfer {
 }
